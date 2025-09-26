65b0417720f632af13ff740f79f2a68caf77e2f2
Leonardo Henrique Pavan <leonardopavancursos@gmail.com>
2025-09-26 19:42:17 -0300

    Iniciado Repositório
    Iniciado repositório para a criação de uma calculadora de imc

---
+++ src/main/java/com/mycompany/calculoimc/CalculoIMC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculoimc;
+
+/**
+ *
+ * @author leonardo.hpavan
+ */
+public class CalculoIMC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
